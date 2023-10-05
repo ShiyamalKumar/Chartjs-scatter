8446e1c786ff133ff7a857e5d653464015ae974a
Shiyamal Asokan <shiyamal.asokan@bounteous.com>
2023-10-05 17:00:50 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import ScatterChartWithZoom from './ScatterChartWithZoom'
-
-
+import React from 'react';
+import ScatterChartWithZoom from './ScatterChartWithZoom';
 
 const App = () => {
   return (
-    <div style={{ width: '800px', height: '400px' }}>
-      <ScatterChartWithZoom />
+    <div>
+      <h1>Chart using Chart.js Plugins</h1>
+      <div style={{ width: '800px', height: '400px' }}>
+        <ScatterChartWithZoom />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
